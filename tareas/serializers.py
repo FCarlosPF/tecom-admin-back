68799336deb819be151bad68c7866603aa3446a3
Freddy Carlos Paucar Flores <carlos.paucar@tecomsg.com.pe>
2024-11-26 18:06:50 -0500

    agregar tareas y eliminar venv

--- tareas/serializers.py
+++ tareas/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 from .models import Tareas, AsignacionesTareas
 from django.utils.timezone import now
 from datetime import datetime
-
+from django.utils import timezone
 
 class TareasSerializer(serializers.ModelSerializer):
     dias_restantes = serializers.SerializerMethodField()
+    dias_pasados = serializers.SerializerMethodField()
 
     class Meta:
         model = Tareas
@@ -19,13 +20,21 @@ class TareasSerializer(serializers.ModelSerializer):
             'prioridad',
             'estado',
             'dias_restantes',
+            'dias_pasados',
+
         ]
 
     def get_dias_restantes(self, obj):
-        if obj.fecha_estimada_fin and obj.fecha_inicio:
-            delta = obj.fecha_estimada_fin - obj.fecha_inicio
+        if obj.fecha_estimada_fin:
+            delta = obj.fecha_estimada_fin - timezone.now().date()
             return max(delta.days, 0)  # Devuelve 0 si ya pasó la fecha
-        return None  
+        return None
+
+    def get_dias_pasados(self, obj):
+        if obj.fecha_estimada_fin:
+            delta = timezone.now().date() - obj.fecha_estimada_fin
+            return max(delta.days, 0)  # Devuelve 0 si aún no ha pasado la fecha
+        return None
 
 class AsignacionesTareasSerializer(serializers.ModelSerializer):
     tarea = TareasSerializer()
