1773750c5f08a0c9c1b99973aaed6f2b5cc42736
Freddy Carlos Paucar Flores <carlos.paucar@tecomsg.com.pe>
2025-01-10 18:04:00 -0500

    10/01/2025

--- administracion_tecom_back/settings.py
+++ administracion_tecom_back/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import dj_database_url
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,7 +67,6 @@ REST_FRAMEWORK = {
 }
 
 
-from datetime import timedelta
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=5),
